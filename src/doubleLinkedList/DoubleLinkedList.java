198c51d17533b34d80c0620cbd78fbf9fa23f2b6
icarnegie07 <icarnegie07@10.1.212.105>
2017-10-24 12:35:46 -0600

    ya no esta rojito

--- src/doubleLinkedList/DoubleLinkedList.java
+++ src/doubleLinkedList/DoubleLinkedList.java
@@ -1,22 +1,49 @@
 package doubleLinkedList;
 
-public class DoubleLinkedList {
+import node.node;
 
-    private node<T> _start=null, _end=null;
+public class DoubleLinkedList<T> {
 
-    public DoubleLinkedList() {
-        _start = new node<T>();
-        _start.setIndex(-1);
-        _end = new node<T>();
-        _end.setIndex(-1);
-    }
+	public node<T> start = null, end = null;
 
-    public DoubleLinkedList(T value) {
-        this();
-        node<T> nuevo = new node<T>(value);
-        _end.setBack(nuevo);
-        //_end.getback().setIndex(0);
-        _start.setNext(_end.getBack());
-        _start.getNext().setIndex(0);
-    }
+	public DoubleLinkedList() {
+		start = new node<T>();
+		start.setIndex(-1);
+		end = new node<T>();
+		end.setIndex(-1);
+	}
+
+	public DoubleLinkedList(T value) {
+		this();
+		node<T> nuevo = new node<T>(value);
+		end.setBack(nuevo);
+		// end.getback().setIndex(0);
+		start.setNext(end.getBack());
+		start.getNext().setIndex(0);
+	}
+
+	public void addEnd(T value) {
+
+	}
+
+	public void addStart(T value) {
+		node<T> tmp = start.getNext();
+		start.setNext(new node<T>(value));
+	}
+
+	public node<T> Search(T value) {
+		return Search(value, start, end);
+
+	}
+
+	private node<T> Search(T value, node<T> start, node<T> end) {
+		if (start.getNext() == null) {
+			return null;
+		} else if (start.getNext().getValue().equals(value)) {
+			return start.getNext();
+		} else if (end.getBack().getValue().equals(value)) {
+			return end.getBack();
+		}
+		return Search(value, start.getNext(), end.getBack());
+	}
 }
--- src/node/node.java
+++ src/node/node.java
@@ -8,65 +8,54 @@
 
 package node;
 
+import java.util.Iterator;
+
 public class node<T> {
-
-        private T value = null;
-        private node <T> next = null;
-        private node <T> back  = null;
-        private long index = 0;
-
-
-
-        public node()
-        {
-            this.value= null;
-        }
-        public long getIndex()
-        {
-            return index;
-        }
-        public long setIndex()
-        {
-            return index;
-        }
-
-        public node (T value)
-        {
-            this.value  = value;
-        }
-
-        public T getValue() {
-            return value;
-        }
-
-
-        public void setValue(T value) {
-            this.value = value;
-        }
-
-
-        public node<T> getNext() {
-            return next;
-        }
-
-
-        public void setNext(node<T> next) {
-            this.next = next;
-        }
-
-
-        public node<T> getBack() {
-            return back;
-        }
-
-
-        public void setBack(node<T> back) {
-            this.back = back;
-        }
-
-        public String toString()
-        {
-            return super.toString();
-        }
-
-    }
+	private T value=null;
+	private node<T> next=null;
+	private node<T> back=null;
+	private long index =0;
+	
+	public long getIndex() {
+		return index;
+	}
+	public void setIndex(long index) {
+		this.index=index;
+	}
+	
+	public node(){
+		this.value= null;
+	}
+	public node(T value) {
+		this.value=value;
+	}
+	
+	public T getValue() {
+		return value;
+	}
+	public void setValue(T value) {
+		this.value = value;
+	}
+	public node<T> getNext() {
+		return next;
+	}
+	public void setNext(node<T> next) {
+		this.next = next;
+	}
+	public node<T> getBack() {
+		return back;
+	}
+	public void setBack(node<T> back) {
+		this.back = back;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	public void setIndex(node<T> indice) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
